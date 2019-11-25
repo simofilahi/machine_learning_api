8aa4e42b197956ebf4e52dfa500334adeebf52fa
simofilahi <mohamedfilahi222@gmail.com>
2019-11-25 18:18:15 +0100

    add app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import * 
+from model_training import model, prediction
+
+app = Flask(__name__)  
+ 
+@app.route('/')  
+def upload():  
+    return render_template("file_upload_form.html")  
+ 
+@app.route('/training', methods = ['POST'])  
+def training():  
+    if request.method == 'POST':  
+        f = request.files['file']  
+        f.save(f.filename) 
+        msg = model(f.filename)
+    return render_template("predict.html") 
+
+@app.route('/predeict', methods = ['POST']) 
+def predict(): 
+    if request.method == 'POST': 
+        f = request.files['file'] 
+        pr = prediction(f.filename)
+        # else:
+        #     return "model not found"
+    #jsonify({'prediction': list(prediction)})
+    return render_template("<p>the operations done successfully</p>")
+if __name__ == '__main__': 
+    app.run(debug = True)  
